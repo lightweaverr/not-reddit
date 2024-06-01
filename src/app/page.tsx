1b473e7a3fc85004df98f87a0375230a21a4234d
Ronit Aryan <shadowsouledx@gmail.com>
2024-06-01 14:42:34 +0530

    TODO: comment next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
-import { buttonVariants } from "@/components/ui/Button";
-import { HomeIcon } from "lucide-react";
-import Link from "next/link";
+import CustomFeed from '@/components/homepage/CustomFeed'
+import GeneralFeed from '@/components/homepage/GeneralFeed'
+import { buttonVariants } from '@/components/ui/Button'
+import { getAuthSession } from '@/lib/auth'
+import { Home as HomeIcon } from 'lucide-react'
+import Link from 'next/link'
 
-export default function Home() {
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
+export default async function Home() {
+  const session = await getAuthSession()
+
   return (
-  <div>
-     <h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
+    <>
+      <h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
-        
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
+
+        {/* subreddit info */}
         <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
           <div className='bg-emerald-100 px-6 py-4'>
             <p className='font-semibold py-3 flex items-center gap-1.5'>
@@ -33,9 +44,6 @@ export default function Home() {
           </dl>
         </div>
       </div>
-  </div>
-)
+    </>
+  )
 }
-
-
-
