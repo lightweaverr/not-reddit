60b473e3d484619d414d5bdfdd9f946975ea5a30
Ronit Aryan <shadowsouledx@gmail.com>
2024-06-03 15:23:09 +0530

    completed search and usename change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import Navbar from '@/components/Navbar'
 import Providers from '@/components/Providers'
-import { Toaster } from '@/components/ui/toaster'
+import { Toaster } from '@/components/ui/Toaster'
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import {Inter} from 'next/font/google'
 
 export const metadata = {
-  title: 'Readit',
+  title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
 
